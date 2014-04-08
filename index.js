e74d2c86edc503d7b7bf5fdc200faf3f17b570ab
fundon <cfddream@gmail.com>
2014-04-08 10:47:18 +0800

    1.0.0

---
+++ index.js
@@ -0,0 +1,57 @@
+/**
+ *  Module dependencies.
+ */
+
+var debug = require('debug')('wukong:markdown');
+var marked = require('marked');
+var path = require('path');
+var basename = path.basename;
+var dirname = path.dirname;
+var extname = path.extname;
+
+/**
+ *  Expose `markdown`.
+ */
+
+module.exports = markdown;
+
+function markdown(options) {
+  options = options || {};
+  var keys = options.keys || [];
+
+  return function *markdown(next) {
+    var files = this.input;
+    var out = this.output;
+    var file, data, dir, name, str;
+    for (file in files) {
+      debug('checking file: %s', file);
+      if (!isMkd(file)) continue;
+      data = files[file];
+      dir = dirname(file);
+      name = basename(file, extname(file)) + '.html';
+      if ('.' != dir) name = dir + '/' + name;
+
+      debug('coverting file: %s', file)
+      str = marked(data.contents.toString(), options);
+      data.contents = new Buffer(str);
+      keys.forEach(function (key) {
+        data[key] = marked(data[key], options);
+      });
+
+      out[name] = data;
+    }
+
+    yield next;
+  };
+}
+
+/**
+ *  Check if a `file` is markdown.
+ *
+ *  @param {String} file
+ *  @return {Boolean}
+ */
+
+function isMkd(file) {
+  return /\.md|\.markdown$/.test(extname(file));
+}
---
+++ test/index.js
@@ -0,0 +1,29 @@
+var assert = require('assert');
+var equal = require('assert-dir-equal');
+var Wukong = require('wukong');
+var markdown = require('..');
+
+describe('wukong-markdown', function(){
+  it('should convert markdown files', function (done) {
+    Wukong('test/fixtures/basic')
+      .use(markdown({
+        smartypants: true
+      }))
+      .build(function *() {
+        equal('test/fixtures/basic/expected', 'test/fixtures/basic/build');
+        done();
+      });
+  });
+
+  it('should allow a "keys" option', function(done){
+    Wukong('test/fixtures/keys')
+      .use(markdown({
+        keys: ['custom'],
+        smartypants: true
+      }))
+      .build(function *(files) {
+        assert.equal('<p><em>a</em></p>\n', files['index.html'].custom);
+        done();
+      });
+  });
+});
